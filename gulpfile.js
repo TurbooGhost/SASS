cfe279992e7fe7ebb9824feec41ef8b5a82c8e55
TurbooGhost <mhmd.turboo@gmail.com>
2023-11-07 14:26:36 -0800

    Ready to use

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("turboo/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["shinobi/**/*.scss"], buildStyles);
+  watch(["turboo/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
